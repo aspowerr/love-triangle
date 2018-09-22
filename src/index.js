f3c2fb5028dd47b285fbf4be378e4f1715d8dc84
aspowerr <31863149+aspowerr@users.noreply.github.com>
2018-09-22 11:43:12 +0300

    love-triangle copmlete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let arr = [0];
+  preferences = arr.concat(preferences);
+  
+  let i;
+  for (i = 1; i <= preferences.length; i++) {
+    let first = preferences[i];
+    let second = preferences[first];
+    let third = preferences[second];
+
+    if
+    ((third == i) && (preferences[first] != first
+        && preferences[second] != second
+        && preferences[third] != third)) 
+        {
+          count++;
+        }
+  }
+  return count / 3;
 };
+
